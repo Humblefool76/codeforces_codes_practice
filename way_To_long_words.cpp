70bebd5a49bbf6bffaf6d7222bc1383a32d1c7da
Humblefool76 <93398938+Humblefool76@users.noreply.github.com>
2022-02-03 18:07:49 +0530

    Update way_To_long_words.cpp

--- way_To_long_words.cpp
+++ way_To_long_words.cpp
@@ -12,7 +12,13 @@ void solve(){
     while(n--){
 
         cin>>s;
-        cout<<s[0]<<(s.size()-2)<<s[s.size()-1]<<e;
+        if(s.size()>10){
+            cout<<s[0]<<(s.size()-2)<<s[s.size()-1]<<e;
+        }
+        else{
+            cout<<s<<e;
+
+        }
     }
 }
 
